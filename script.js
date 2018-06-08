bc2b56d1fbb93f92434ca314b66ff216f3b079c1
Ariies <ankludka@gmail.com>
2018-06-08 22:53:41 +0200

    added login and register functions

--- script.js
+++ script.js
@@ -48,9 +48,13 @@ function isEmpty(field){
 //TODO FIX validation
 //TODO extend it to both login and register
 //TODO display responses somewhere (not an alert...)
-document.getElementById("loginButton").addEventListener("click", function(){
+//Cookies or sessions to stay logged 
+//TODO block access to /training when direct linking and not logged in
+document.getElementById("loginButton").addEventListener("click", login);
+document.getElementById("registerButton").addEventListener("click", register);
 
-   
+
+function login(){
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "./login.php", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -59,19 +63,39 @@ document.getElementById("loginButton").addEventListener("click", function(){
     let password = document.getElementById('password').value;
     let stay = (document.getElementById('stayLogged').checked ? 1 : 0);
 
-    xhr.send("email="+email+"&password="+password+"&stayLogged="+stay+"&register=1");
+    xhr.send("email="+email+"&password="+password+"&stayLogged="+stay+"&login=1");
 
     xhr.addEventListener('load', function() {
         if (this.status === 200) {
-//TODO change response to login/register
+            //TODO change response
+            if(xhr.responseText == "success")
+               window.location.href = "./training";
+            else
+                alert(xhr.responseText);
+        }
+        else
+            alert("Error code " + this.status);
+    })
+    
+}
+
+
+function register(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "./register.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    xhr.send("email="+email+"&password="+password+"&register=1");
+
+    xhr.addEventListener('load', function() {
+        if (this.status === 200) {
+            //TODO change response
             alert(xhr.responseText);
         }
         else
-        alert("something fucky");
+            alert("Error code " + this.status);
     })
-
-
-});
-
-
-
+}
