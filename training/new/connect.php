d748aab01a775c58f8ac97d90d1328ff086816c2
anklu <ankludka@gmail.com>
2018-11-26 21:13:39 +0100

    day/exercise data structure idea change

--- training/new/connect.php
+++ training/new/connect.php
@@ -11,6 +11,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+/*
+
 class day {
     var $dayNumber;
     var $dayName;
@@ -47,14 +49,15 @@ class exercise {
 }
 
 
-
+ 
 $day1 = new day(1, "A1", "Squat day");
 $day2 = new day(2, "B1", "OHP day");
 $day3 = new day(3, "A2", "Bench Press day");
 $day4 = new day(4, "B2", "Deadlift day");
 
+//A1
 array_push($day1->exerciseList, new exercise("Squats", 20, 1, 0, 1, TRUE, 5, FALSE));
-array_push($day1->exerciseList, new exercise("Bench Press", 20, 2, 0, 2, FALSE, 5, FALSE));
+array_push($day1->exerciseList, new exercise("Bench Press", 20, 2, 0, 2, FALSE, 2.5, FALSE));
 array_push($day1->exerciseList, new exercise("Lat Pulldown", 10, 3, 0, 3, TRUE, 2.5, FALSE));
 array_push($day1->exerciseList, new exercise("Chin Ups", 0, 3, 0, 4, TRUE, 0, FALSE));
 array_push($day1->exerciseList, new exercise("Curls", 6, 3, 0, 5, TRUE, 1, FALSE));
@@ -63,10 +66,42 @@ array_push($day1->exerciseList, new exercise("Leg Raises", 0, 3, 0, 7, TRUE, 0, 
 array_push($day1->exerciseList, new exercise("Cable Crunches", 5, 3, 0, 8, TRUE, 1, FALSE));
 array_push($day1->exerciseList, new exercise("Forearm Set", 15, 3, 0, 9, FALSE, 5, FALSE));
 
-//TODO day2,3,4
+//B1
+array_push($day2->exerciseList, new exercise("Overhead Press", 20, 1, 0, 19, FALSE, 2.5, FALSE));
+array_push($day2->exerciseList, new exercise("Deadlift", 40, 2, 0, 20, TRUE, 5, FALSE));
+array_push($day2->exerciseList, new exercise("Dumbbell row", 10, 3, 0, 21, TRUE, 1, FALSE));
+array_push($day2->exerciseList, new exercise("Chin Ups", 0, 3, 0, 4, TRUE, 0, FALSE));
+array_push($day2->exerciseList, new exercise("Curls", 6, 3, 0, 5, TRUE, 1, FALSE));
+array_push($day2->exerciseList, new exercise("Close-Grip Bench Press", 10, 3, 0, 24, TRUE, 2.5, FALSE));
+array_push($day2->exerciseList, new exercise("Leg Raises", 0, 3, 0, 7, TRUE, 0, FALSE));
+array_push($day2->exerciseList, new exercise("Cable Crunches", 5, 3, 0, 8, TRUE, 1, FALSE));
+array_push($day2->exerciseList, new exercise("Forearm Set", 15, 3, 0, 9, FALSE, 5, FALSE));
+
+//A2
+array_push($day3->exerciseList, new exercise("Bench Press", 20, 1, 0, 10, FALSE, 2.5, FALSE));
+array_push($day3->exerciseList, new exercise("Squats", 20, 2, 0, 11, TRUE, 5, FALSE));
+array_push($day3->exerciseList, new exercise("Lat Pulldown", 10, 3, 0, 3, TRUE, 2.5, FALSE));
+array_push($day3->exerciseList, new exercise("Chin Ups", 0, 3, 0, 13, TRUE, 0, FALSE));
+array_push($day3->exerciseList, new exercise("Curls", 6, 3, 0, 14, TRUE, 1, FALSE));
+array_push($day3->exerciseList, new exercise("Tricep Extensions", 10, 3, 0, 6, TRUE, 2.5, FALSE));
+array_push($day3->exerciseList, new exercise("Leg Raises", 0, 3, 0, 7, TRUE, 0, FALSE));
+array_push($day3->exerciseList, new exercise("Cable Crunches", 5, 3, 0, 8, TRUE, 1, FALSE));
+array_push($day3->exerciseList, new exercise("Forearm Set", 15, 3, 0, 9, FALSE, 5, FALSE));
+
+//B2
+array_push($day4->exerciseList, new exercise("Deadlift", 20, 1, 0, 28, FALSE, 2.5, FALSE));
+array_push($day4->exerciseList, new exercise("Overhead Press", 40, 2, 0, 29, TRUE, 5, FALSE));
+array_push($day4->exerciseList, new exercise("Dumbbell row", 10, 3, 0, 30, TRUE, 1, FALSE));
+array_push($day4->exerciseList, new exercise("Chin Ups", 0, 3, 0, 31, TRUE, 0, FALSE));
+array_push($day4->exerciseList, new exercise("Curls", 6, 3, 0, 32, TRUE, 1, FALSE));
+array_push($day4->exerciseList, new exercise("Close-Grip Bench Press", 10, 3, 0, 33, TRUE, 2.5, FALSE));
+array_push($day4->exerciseList, new exercise("Leg Raises", 0, 3, 0, 7, TRUE, 0, FALSE));
+array_push($day4->exerciseList, new exercise("Cable Crunches", 5, 3, 0, 8, TRUE, 1, FALSE));
+array_push($day4->exerciseList, new exercise("Forearm Set", 15, 3, 0, 9, FALSE, 5, FALSE));
 
 
 
+*/
 
 
 /*
