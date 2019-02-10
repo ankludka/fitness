78625f4b14e148a1f5c1f579b05333826da02659
anklu <ankludka@gmail.com>
2019-02-10 19:40:24 +0100

    changed to object oriented mysqli

--- register.php
+++ register.php
@@ -2,38 +2,36 @@
 //TODO fix responses
 
 
-session_start();
+$servername = "localhost";
+$username = "ariies1_fitness";
+$password = "fitpass1234";
+$dbname = "ariies1_fitness";
 
-$link = mysqli_connect("localhost", "ariies1_fitness", "fitpass1234", "ariies1_fitness");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if (!$link) {
-  echo "Error: Unable to connect to MySQL." . PHP_EOL;
-  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-  exit;
-}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 
 
 
 if( array_key_exists("register", $_POST))
 {
-    $email = mysqli_real_escape_string($link, $_POST['email']);
-    $password = mysqli_real_escape_string($link, $_POST['password']);
+    $email = $conn -> real_escape_string($_POST['email']);
+    $password = $conn -> real_escape_string($_POST['password']);
 
     //Check if email is already taken
-    $query = "SELECT id FROM users WHERE email ='".$email."';";
-    $result = mysqli_query($link, $query);
+    $result = $conn -> query("SELECT id FROM users WHERE email ='".$email."';");
 
-    if(mysqli_num_rows($result) > 0)
+    if($result -> num_rows)
         echo "This e-mail address is already registered.";
     else
     {
         //TODO maybe some e-mail confirmation?
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $query = "INSERT INTO `users` (`email`, `password`) VALUES ('".$email."', '".$hashedPassword."');";
-
-        if(!mysqli_query($link, $query))
+        if(!$conn-> query("INSERT INTO `users` (`email`, `password`) VALUES ('".$email."', '".$hashedPassword."');"))
             echo "Registration failed - please try again later.";
         else {
             echo "You have been registered!";
