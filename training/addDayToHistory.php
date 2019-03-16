aaaa38fdbd23322888fcfc791e1c5490e9bfa54b
anklu <ankludka@gmail.com>
2019-03-16 22:11:45 +0100

    fixed query bugs

--- training/addDayToHistory.php
+++ training/addDayToHistory.php
@@ -12,22 +12,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 $userId = $_POST['userId'];
 $dayId = $_POST['dayId'];
 $date = $_POST['date'];
 
-$dayExists = $conn -> query('SELECT COUNT(*) FROM historyOfDays WHERE dayId = '.$dayId.'AND userId = '.$userId) -> fetch_array(MYSQLI_NUM)[0];
+
+$dayExists = $conn -> query("SELECT COUNT(*) FROM historyOfDays WHERE dayId = ".$dayId." AND userId = ".$userId) -> fetch_array(MYSQLI_NUM)[0];
+
 
 if ($dayExists){
-    $conn -> query('UPDATE historyOfDays SET dateOfCompletion = '.$date);
+    $conn -> query("UPDATE historyOfDays SET dateOfCompletion = '".$date."' WHERE dayId = ".$dayId." AND userId = ".$userId);
 }
 else{
-    $conn -> query('INSERT INTO historyOfDays VALUES'.$dayId.', '.$userId.', '.$date);
+    $conn -> query("INSERT INTO historyOfDays(dayId, userId, dateOfCompletion) VALUES ('".$dayId."', '".$userId."', '".$date."')");
 }
-
-
-
-
 
 
 $conn->close();
