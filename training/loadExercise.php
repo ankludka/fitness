5df9a1bedcbb673aa3e2f564fe916adc528ee206
anklu <ankludka@gmail.com>
2019-03-21 21:52:09 +0100

    lastday query bug fixes and name changes

--- training/loadExercise.php
+++ training/loadExercise.php
@@ -14,10 +14,17 @@ if ($conn->connect_error) {
 
 $email = $_SESSION["email"];
 
-$lastDay = $conn -> query("SELECT MAX(day.dayId) FROM day JOIN users ON day.userId = users.id WHERE users.email = '".$email."'") -> fetch_array(MYSQLI_NUM)[0];
+$q = "SELECT MIN(dayId) as lastDay FROM
+    (SELECT day.dayId FROM day JOIN users ON day.userId = users.id
+    WHERE users.email = '".$email."' AND day.dayId NOT IN (
+    SELECT historyOfDays.dayId FROM historyOfDays)) AS x";
 
+$lastDay = $conn -> query($q) -> fetch_array(MYSQLI_NUM)[0];
 
-$day = "SELECT * FROM day JOIN users ON day.userId = users.id WHERE users.email = '".$email."'AND day.dayId =".$lastDay;
+if ($lastDay == NULL){
+    die("day was null");
+}
+
 
 $result = $conn -> query("SELECT day.dayId, day.dayNumber, program.dayName, exercise_dictionary.name AS 'exerciseName', day.exerciseId, day.exerciseWeight, exercise.tier AS 'exerciseTier', day.exerciseFailCount, day.exerciseSuccess, exercise.lastSetAmrap as 'exerciseLastSetAmrap', exercise.weightIncrement AS 'exerciseWeightIncrement', day.exerciseCompleted, users.email, users.id AS 'userId'
 FROM day JOIN exercise_dictionary ON day.exerciseId = exercise_dictionary.id JOIN users ON day.userId = users.id JOIN program ON day.dayNumber = program.dayNumber JOIN exercise ON day.exerciseId = exercise.id
